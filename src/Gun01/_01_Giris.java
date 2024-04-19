343e87bfef1f2197880de73e6b52cf85efa3c472
kudencagri <Kudencagri@gmail.com>
2024-04-19 16:42:07 +0300

    TestNG

--- src/Gun01/_01_Giris.java
+++ src/Gun01/_01_Giris.java
@@ -1,4 +1,41 @@
 package Gun01;
 
+import org.testng.annotations.Test;                    //*************************************************************buralaardan
+
 public class _01_Giris {
+
+//    public static void main(String[] args) {
+//        DriverIslemleriniYap();
+//        WebSitesiniAc();
+//        LoginTestiniYap();
+//    }
+
+//    public void Test1(){
+        //        DriverIslemleriniYap();             //*** içerden yaparsan testi başlatırsan sadece o testi yapar  yukardan yılzdızlı yerden yaparsan hepsini yapar.
+//        WebSitesiniAc();
+//        LoginTestiniYap();
+   //
+    @Test (priority = 1)   // default=0 aynı seviyeler yine alfabetik çalışıyor.
+    public void DriverIslemleriniYap(){
+        System.out.println("driverIslemleri yapıldı");
+
+    }
+    @Test(priority = 2)
+    public void WebSitesiniAc(){
+        System.out.println("webIslemleri yapıldı");
+    }
+    @Test(priority = 3)
+    public void LoginTestitiniYap(){
+        System.out.println("LoginIslemleri yapıldı");
+    }
+    @Test(priority = 4)
+    public void driverKapat(){
+        System.out.println("driverkapttoIslemleri yapıldı");
+    }
+
+
+
+
 }
+
+
---
+++ src/Gun01/_02_Annotations.java
@@ -0,0 +1,20 @@
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class _02_Annotations {
+    @Test // Annotations :metodun türünü ve çalışma özelliklerini belirtir.
+    public void Test1(){                    // test1 : test metodunun adı
+        System.out.println("Test 1 çalıştı");
+    }
+
+    @Test // Annotations :metodun türünü ve çalışma özelliklerini belirtir.
+    public void Test2(){// test1 : test metodunun adı
+        System.out.println("Test 2 çalıştı");
+    }
+
+
+
+
+
+}
