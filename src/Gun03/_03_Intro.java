0b8c05c5d9e3ca9c6165fad9a204902a7368e298
kudencagri <Kudencagri@gmail.com>
2024-04-24 17:36:06 +0300

    TestNG

--- src/Gun03/_03_Intro.java
+++ src/Gun03/_03_Intro.java
@@ -1,4 +1,11 @@
 package Gun03;
 
+import org.testng.annotations.*;
+
 public class _03_Intro {
+   @Test
+    public void Test1(){System.out.println("Test Çalışması 2");}
+
+    @Test
+    public void Test2(){System.out.println("Test Çalışması 2");}
 }
