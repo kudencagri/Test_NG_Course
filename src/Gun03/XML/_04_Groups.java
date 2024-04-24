7499cd0628e6f627ef635e28519287116507e565
kudencagri <Kudencagri@gmail.com>
2024-04-24 17:47:26 +0300

    TestNG

--- src/Gun03/XML/_04_Groups.java
+++ src/Gun03/XML/_04_Groups.java
@@ -9,14 +9,19 @@ public class _04_Groups {
 
     @Test(groups = {"Regression"})
     public void Test3(){System.out.println("Test3");}
+
     @Test(groups = {"Regression"})
     public void Test5(){System.out.println("Test5");}
+
     @Test(groups = {"Regression"})
     public void Test2(){System.out.println("Test2");}
+
     @Test(groups = {"Smoke Test"})
     public void Test4(){System.out.println("Test4");}
+
     @Test(groups = {"Smoke Test"})
     public void Test6(){System.out.println("Test6");}
+
     @Test
     public void Test7(){System.out.println("Test 7");}
 
