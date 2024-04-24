53d0f9be77b1c85c71ca61ec9d255d616c915f0d
kudencagri <Kudencagri@gmail.com>
2024-04-24 10:57:22 +0300

    TestNG

--- src/Gun02/_05_SubscribeNewsLetter.java
+++ src/Gun02/_05_SubscribeNewsLetter.java
@@ -1,9 +1,9 @@
 package Gun02;
 
 import Utility.BaseDriver;
-import org.openqa.selenium.By;            // siteyi aç newsletter subscirebe işlemini abone olunuz(Yes) işlemini başarılı olduğunu kontorl ediniz
-                                            // farklı işlemle abonelikten çıkın
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;            // 1)siteyi aç newsletter subscirebe işlemini abone olunuz(Yes) işlemini başarılı olduğunu kontorl ediniz
+                                            // 2)farklı işlemle abonelikten çıkın
+import org.openqa.selenium.WebElement;     // 3) konttol edin no ise yes yes ise no yapın
 import org.testng.Assert;
 import org.testng.annotations.Test; // login işlemi base drivera konuru çünkü before classa konur çünkü hertesten önce yapar. **************
 
@@ -15,8 +15,8 @@ public class _05_SubscribeNewsLetter extends BaseDriver {
         newsLetterLink.click();
 
 
-        WebElement SubscribeNO=driver.findElement(By.xpath("//input[@value='1']"));
-        SubscribeNO.click();
+        WebElement SubscribeYes=driver.findElement(By.xpath("//input[@value='1']"));
+        SubscribeYes.click();
 
         WebElement contuniue=driver.findElement(By.xpath("//input[@value='Continue']"));
         contuniue.click();
@@ -31,8 +31,24 @@ public class _05_SubscribeNewsLetter extends BaseDriver {
         newsLetterLink.click();
 
 
-        WebElement SubscribeYes=driver.findElement(By.xpath("//input[@value='0']"));
-        SubscribeYes.click();
+        WebElement SubscribeNo=driver.findElement(By.xpath("//input[@value='0']"));
+        SubscribeNo.click();
+
+        WebElement contuniue=driver.findElement(By.xpath("//input[@value='Continue']"));
+        contuniue.click();
+
+        WebElement AssertControl=driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+        Assert.assertTrue(AssertControl.getText().toLowerCase().contains("success"));
+    }
+    @Test
+    public void NewsLetterChange(){
+        WebElement newsLetterLink=driver.findElement(By.linkText("Newsletter"));
+        newsLetterLink.click();
+
+        WebElement SubscribeYes=driver.findElement(By.xpath("//input[@value='1']"));
+        WebElement SubscribeNO=driver.findElement(By.xpath("//input[@value='0']"));
+
+
 
         WebElement contuniue=driver.findElement(By.xpath("//input[@value='Continue']"));
         contuniue.click();
